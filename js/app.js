c4e62b4476c4b6f58f60ce2adaa9026cb5eae8fa
Sumayea Rahman <shorbonam104@gmail.com>
2023-03-27 20:39:20 +0600

    cards

--- js/app.js
+++ js/app.js
@@ -8,8 +8,22 @@ const loadPhones = async() =>{
 const displayPhones = phones =>{
     // console.log(phones);
     const phonesContainer = document.getElementById('phone-container');
-    phones.array.forEach(phone => {
+    phones.forEach(phone => {
+        const phoneDiv = document.createElement('div');
+        phoneDiv.classList.add('col');
+        phoneDiv.innerHTML = `
+        <div class="card">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                                additional content. This content is a little bit longer.</p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                    </div>
         
+        `;
+        phonesContainer.appendChild(phoneDiv);
     });
 }
 loadPhones();
