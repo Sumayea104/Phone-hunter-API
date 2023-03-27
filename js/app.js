12096297b738b23513b9fb82d228c2b37f570611
Sumayea Rahman <shorbonam104@gmail.com>
2023-03-27 20:29:02 +0600

    basic

---
+++ js/app.js
@@ -0,0 +1,15 @@
+const loadPhones = async() =>{
+    const url = `https://openapi.programming-hero.com/api/phones?search=iphone`
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhones(data.data);
+}
+
+const displayPhones = phones =>{
+    // console.log(phones);
+    const phonesContainer = document.getElementById('phone-container');
+    phones.array.forEach(phone => {
+        
+    });
+}
+loadPhones();
